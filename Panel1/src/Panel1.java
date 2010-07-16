cb9ca45f907dde80dc20e6a0a67f84e6518fc67c
broli <carlos.ferrabone@gmail.com>
2010-07-16 16:45:29 -0300

    ignores

--- Panel1/src/Panel1.java
+++ Panel1/src/Panel1.java
@@ -24,10 +24,13 @@ public class Panel1 {
 	public void go() {
 		JFrame frame = new JFrame () ;
 		JPanel panel = new JPanel () ;
-		JButton button1 = new JButton ("shock me");
+		JButton button1 = new JButton ("shock me0");
 		JButton button2 = new JButton ("shock me2");
 		JButton button3 = new JButton ("shock me3");
 		JButton button4 = new JButton ("shock me4");
+		JButton button5 = new JButton ("shock me5");
+		JButton button6 = new JButton ("shock me6");
+		JButton button7 = new JButton ("shock me7");
 		
 		panel.setBackground(Color.darkGray);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -36,6 +39,9 @@ public class Panel1 {
 		panel.add(button2);
 		panel.add(button3);
 		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		panel.add(button7);
 		
 		frame.getContentPane().add(BorderLayout.EAST, panel);
 		
